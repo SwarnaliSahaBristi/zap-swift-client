0cd59bc90d28d76c6e68a67f37c75109588b6351
SwarnaliSahaBristi <swarnalisahabristy@gmail.com>
2025-11-28 22:13:43 +0600

    rider approve added

--- src/pages/Dashboard/ApproveRiders.jsx
+++ src/pages/Dashboard/ApproveRiders.jsx
@@ -1,11 +1,102 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { FaTrashAlt, FaUserCheck } from "react-icons/fa";
+import { IoPersonRemove } from "react-icons/io5";
+import Swal from "sweetalert2";
 
 const ApproveRiders = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const axiosSecure = useAxiosSecure();
+  const { refetch, data: riders = [] } = useQuery({
+    queryKey: ["riders", "pending"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/rider");
+      return res.data;
+    },
+  });
+
+  const updateRiderStatus = (rider, status) => {
+    const updateInfo = { status: status, email: rider.riderEmail };
+    axiosSecure.patch(`/rider/${rider._id}`, updateInfo).then((res) => {
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `Rider status is set to ${status}`,
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      }
+    });
+  };
+
+  const handleApproval = (rider) => {
+    updateRiderStatus(rider, "approved");
+  };
+
+  const handleRejection = (rider) => {
+    updateRiderStatus(rider, "rejected");
+  };
+
+  return (
+    <div>
+      <h2 className="text-5xl">Pending Request: {riders.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Status</th>
+              <th>District</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {riders.map((rider, index) => (
+              <tr key={rider._id}>
+                <th>{index + 1}</th>
+                <td>{rider.riderName}</td>
+                <td>{rider.riderEmail}</td>
+                <td>
+                  <p
+                    className={`${
+                      rider.status === "approved"
+                        ? "text-green-800"
+                        : "text-red-500"
+                    }`}
+                  >
+                    {rider.status}
+                  </p>
+                </td>
+                <td>{rider.riderDistrict}</td>
+                <td>
+                  <button
+                    onClick={() => handleApproval(rider)}
+                    className="btn btn-square hover:bg-primary"
+                  >
+                    <FaUserCheck />
+                  </button>
+                  <button
+                    onClick={() => handleRejection(rider)}
+                    className="btn btn-square hover:bg-primary mx-2"
+                  >
+                    <IoPersonRemove />
+                  </button>
+                  <button className="btn btn-square hover:bg-primary">
+                    <FaTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ApproveRiders;
